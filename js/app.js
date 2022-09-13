e28c065ad9eb0e7606d44ff08f29c03630e4073b
Abdyl-Aziz <azizshihabov@mail.ru>
2022-09-13 16:12:03 +0300

    feat: add project

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
-import '../sass/style.scss'
-import {
-  quoteContainer,
-  twitterBtn,
-  quoteText,
-  quoteAuthor,
-  nextBtn,
-  loader,
-  progressBar
-} from './DOMElements.js';
-import '../vendor/typetura.min.js';
+const quoteContainer = document.querySelector('.container');
+const twitterBtn = document.querySelector('.quote__twitter');
+const quoteText = document.querySelector('.quote__text');
+const quoteAuthor = document.querySelector('.quote__author');
+const nextBtn = document.querySelector('.quote__next');
+const loader = document.querySelector('.loader');
+const progressBar = document.querySelector('.quote__progress');
+
 
 function tweetQuote() {
   const quote = quoteText.innerText;
